65436edef5e1a64d3312255eb2b41de808663b27
Kristoffer <Kripe22@student.sdu.dk>
2025-03-10 13:43:41 +0100

    Bank entity added

---
+++ Database/BankEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bach2025_nortec.Database;
+
+public class BankEntity
+{
+    [Key]
+    public int BId{ get; set; }
+    public string? name { get; set; }
+}
