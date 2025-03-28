2e481e214442b9419afd93f64c7c824ac5724703
Kristoffer <Kripe22@student.sdu.dk>
2025-03-28 07:49:55 +0100

    Minor changed to id and @Value

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using MySql.EntityFrameworkCore.Extensions;
 using DotNetEnv;
 using System.Net.Http;
+using BlazorTest.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,9 @@ Env.Load();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<DataAnalysisService>();
+
 
 // Configure MySQL
 var connectionString = $"Server={Env.GetString("DATABASE_HOST")};Database={Env.GetString("DATABASE_NAME")};User={Env.GetString("DATABASE_USERNAME")};Password={Env.GetString("DATABASE_PASSWORD")};";
--- Services/DataAnalysisService.cs
+++ Services/DataAnalysisService.cs
@@ -1,19 +1,58 @@
-﻿namespace BlazorTest.Services;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class DataAnalysisService
+namespace BlazorTest.Services
 {
-    private readonly YourDbContext _dbContext;
-
-    public DataAnalysisService(YourDbContext dbContext)
+    public class DataAnalysisService
     {
-        _dbContext = dbContext;
-    }
+        private readonly YourDbContext _dbContext;
 
-    public decimal CalculateTotalRevenueForBank(int bankId)
-    {
-        return _dbContext.Transactions
-            .Where(t => _dbContext.Laundromat
-                .Any(l => l.bId == bankId && l.kId == t.LaundromatId))
-            .Sum(t => Math.Abs((Convert.ToDecimal(t.amount)))) / 100;
+        public DataAnalysisService(YourDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public decimal CalculateTotalRevenueForBank(int bankId)
+        {
+            return _dbContext.Transactions
+                .Where(t => _dbContext.Laundromat
+                    .Any(l => l.bId == bankId && l.kId == t.LaundromatId))
+                .Sum(t => Math.Abs(Convert.ToDecimal(t.amount))) / 100;
+        }
+
+        public class ChartDataPoint
+        {
+            public string Label { get; set; }
+            public decimal Value { get; set; }
+        }
+        public async Task<List<ChartDataPoint>> GetRevenueForAllLaundromatsInBank(int bankId)
+        {
+            var laundromats = await _dbContext.Laundromat
+                .AsNoTracking()
+                .Where(l => l.bId == bankId)
+                .Select(l => new { l.kId, l.name })
+                .ToListAsync();
+
+
+            var results = new List<ChartDataPoint>();
+
+            // Then, perform a separate query per laundromat
+            foreach (var l in laundromats)
+            {
+                var revenue = await _dbContext.Transactions
+                    .Where(t => t.LaundromatId == l.kId)
+                    .SumAsync(t => Math.Abs(Convert.ToDecimal(t.amount))) / 100;
+
+                results.Add(new ChartDataPoint
+                {
+                    Label = l.name ?? $"ID {l.kId}",
+                    Value = revenue
+                });
+            }
+
+            return results;
+        }
+
+
+
     }
 }
